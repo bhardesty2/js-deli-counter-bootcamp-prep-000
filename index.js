39fc0ef9659cdeabdf5515ddf86ae0fa23be92bf
Blake Hardesty <ha1003218@email.ccbcmd.edu>
2019-01-22 19:18:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function takeANumber(katzDeliLine, Ada){
 }
 
 var takeANumber = function (katzDeliLine, newCustomer){
-  console.log("hi", newCustomer);
   if (katzDeliLine.length > 0);
   katzDeliLine.push(newCustomer);
   return ('Welcome, ' + newCustomer +'. You are number ' + katzDeliLine.length+' in line.')
